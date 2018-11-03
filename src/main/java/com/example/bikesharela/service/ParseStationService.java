c86647ddc13e59f2e70361fc6644bbf3fcebec12
ady8 <ady8@cornell.edu>
2018-11-03 11:06:54 -0400

    parsing for station info

---
+++ src/main/java/com/example/bikesharela/service/ParseStationService.java
@@ -0,0 +1,47 @@
+package com.example.bikesharela.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.AbstractMap;
+
+import com.example.bikesharela.model.StationData;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import com.example.bikesharela.model.BikeShareData;
+
+@Service
+public class ParseStationService {
+
+    public AbstractMap<Integer, StationData> loadStations(String fname) throws FileNotFoundException, IOException {
+
+        HashMap<Integer, StationData> stations = new HashMap<>();
+
+        File f = new ClassPathResource(fname).getFile();
+        CSVParser parser = new CSVParser(new FileReader(f), CSVFormat.DEFAULT.withHeader());
+
+        for (CSVRecord record : parser) {
+
+            Integer stationId = Integer.valueOf(record.get("Station_ID"));
+
+            StationData station = new StationData(
+                    stationId,
+                    record.get("Station_Name"),
+                    record.get("Go_live_date"),
+                    record.get("Region"),
+                    record.get("Status")
+            );
+
+            stations.put(stationId, station);
+        }
+        parser.close();
+
+        return stations;
+    }
+}
